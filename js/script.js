7c2bbabaaffc9ec36e3b721c4f17c3934aaffce8
Habiba <habibawael02@gmail.com>
2022-08-21 19:12:20 +0200

    include code

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const wordTxt = document.querySelector(".word"),
+hintTxt = document.querySelector(".hint span"),
+inputField = document.querySelector("input"),
+refeshBtn = document.querySelector(".refresh-word"),
+timeTxt = document.querySelector(".time b"),
+checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval( () => {
+        if(maxTime>0){
+            maxTime--; //decrement maxTime by 1
+            return timeTxt.innerText = maxTime;
+        }
+        alert(`Time off! ${userWord.toUpperCase( )} was the correct word`);//timer is finish
+        initGame(); //call initGame to restart
+    }, 1000);
+}
+
+const initGame = () => {
+    initTimer(30) //call initTimer with passing 30 as maxTime
+    let randomObj = words[Math.floor(Math.random() * words.length)]; //get random object
+    let wordArray = randomObj.word.split("");//split letters
+    for (let i = wordArray.length-1; i > 0 ; i--) {
+        let j = Math.floor(Math.random() * (i + 1));//get randomm number
+        [wordArray[i],wordArray[j]] = [wordArray[j],wordArray[i]]//swip letters randomly
+    }
+    wordTxt.innerText = wordArray.join("");//pass shuffle word as word
+    hintTxt.innerText = randomObj.hint; //pass random object hint as jint
+    correctWord = randomObj.word.toLowerCase(); //pass random word to correctWord
+    inputField.value="";// input field empty
+    inputField.setAttribute("maxlength", correctWord.length) //set input maxlength attr value to word length
+    console.log(randomObj);
+}
+initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLocaleLowerCase();//get user value
+    if(!userWord) return alert(`Please enter a word`);//if user do not enter anything
+    if(userWord !== correctWord) return alert(`Ooops! ${userWord.toUpperCase()} is not the correct word`);//if word does not match the correct one
+    alert(`Congrats! ${userWord.toUpperCase( )} is the correct word`);//if user enter the correct word
+    initGame();
+}
+checkWord();
+
+refeshBtn.addEventListener("click",initGame);//refresh on refresh button click
+checkBtn.addEventListener("click",checkWord);//check on check button click
---
+++ js/words.js
@@ -0,0 +1,155 @@
+let words = [
+{
+    word:"happy",
+    hint:"feeling or showing pleasure or contentment."
+},
+{
+    word:"charismatic",
+    hint:"possessing an extraordinary ability to attract."
+},{
+    word:"torture",
+    hint:"infliction of suffering to punish or obtain information"
+},{
+    word:"ambiguity",
+    hint:"The quality of being open to more that one interpretation; inexactness."
+},{
+    word:"famine",
+    hint:"a severe shortage of food resulting in starvation and death."
+},{
+    word:"barista",
+    hint:"A person who is job involves preparing different types of coffee."
+},{
+    word:"alfresco",
+    hint:"taking place or located in the open air"
+},{
+    word:"bugbear",
+    hint:" any source, real or imaginary, of needless fright or fear"
+},{
+    word:"calamity",
+    hint:"a great misfortune or disaster"
+},{
+    word:"demitasse",
+    hint:"a small cup for serving strong black coffee after dinner"
+},{
+    word:"gambit",
+    hint:"a device, action, or opening remark"
+},{
+    word:"manifesto",
+    hint:"a public declaration of intentions, opinions, objectives, or motives, as one issued by a government, sovereign, or organization"
+},{
+    word:"quibble",
+    hint:"arguments to evade a point at issue"
+},{
+    word:"interscribe",
+    hint:"to write between"
+},{
+    word:"zephyr",
+    hint:"a calm, gentle breeze."
+},{
+    word:"fika",
+    hint:"a moment to slow down and appreciate the little things in life. "
+},{
+    word:"reverie",
+    hint:"the state of being lost in your own thoughts; a daydream."
+},{
+    word:"beamish",
+    hint:"bright, cheerful, and optimistic."
+},{
+    word:"ineffable",
+    hint:"cannot be described in words"
+},{
+    word:"sumptuous",
+    hint:"magnificent, extravagant, splendid"
+},{
+    word: "addition",
+    hint: "The process of adding numbers"
+},
+{
+    word: "meeting",
+    hint: "Event in which people come together"
+},
+{
+    word: "number",
+    hint: "Math symbol used for counting"
+},
+{
+    word: "exchange",
+    hint: "The act of trading"
+},
+{
+    word: "canvas",
+    hint: "Piece of fabric for oil painting"
+},
+{
+    word: "garden",
+    hint: "Space for planting flower and plant"
+},
+{
+    word: "position",
+    hint: "Location of someone or something"
+},
+{
+    word: "feather",
+    hint: "Hair like outer covering of bird"
+},
+{
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation"
+},
+{
+    word: "tongue",
+    hint: "The muscular organ of mouth"
+},
+{
+    word: "expansion",
+    hint: "The process of increase or grow"
+},
+{
+    word: "country",
+    hint: "A politically identified region"
+},
+{
+    word: "group",
+    hint: "A number of objects or persons"
+},
+{
+    word: "taste",
+    hint: "Ability of tongue to detect flavour"
+},
+{
+    word: "store",
+    hint: "Large shop where goods are traded"
+},
+{
+    word: "field",
+    hint: "Area of land for farming activities"
+},
+{
+    word: "friend",
+    hint: "Person other than a family member"
+},
+{
+    word: "pocket",
+    hint: "A bag for carrying small items"
+},
+{
+    word: "needle",
+    hint: "A thin and sharp metal pin"
+},
+{
+    word: "expert",
+    hint: "Person with extensive knowledge"
+},
+{
+    word: "statement",
+    hint: "A declaration of something"
+},
+{
+    word: "second",
+    hint: "One-sixtieth of a minute"
+},
+{
+    word: "library",
+    hint: "Place containing collection of books"
+}
+]
